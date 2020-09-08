de6f2197676306e44e1ac3d07c403a6e7c32153e
rusal <teacher@C426U00>
2020-09-08 16:53:56 +0300

    sound mario

--- PR01/Z01/1/Program.cs
+++ PR01/Z01/1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace pr01_z1_1
 {
@@ -23,7 +24,36 @@ namespace pr01_z1_1
 
     static void Main(string[] args)
     {
-      Person p = new Person("Tony", "Allen ", 32); Console.WriteLine(p);
+      Person p = new Person("Tony", "Allen ", 32); 
+      
+      Console.WriteLine(p);
+      Console.WriteLine("Hello Mario!");
+      double ton = 0.65f;
+      double times = 0.65f;
+      Console.Beep((int)(ton * 659), (int)(times * 125));
+      Console.Beep((int)(ton * 659), (int)(times * 125));
+      Thread.Sleep((int)(times * 125));
+      Console.Beep((int)(ton * 659), (int)(times * 125));
+      Thread.Sleep(167);
+      Console.Beep((int)(ton * 523), (int)(times * 125));
+      Console.Beep((int)(ton * 659), (int)(times * 125));
+      Thread.Sleep((int)(times * 125));
+      Console.Beep((int)(ton * 784), (int)(times * 125));
+      Thread.Sleep(375);
+      Console.Beep((int)(ton * 392), (int)(times * 125));
+      Thread.Sleep(375);
+      Console.Beep((int)(ton * 523), (int)(times * 125));
+      Thread.Sleep(250);
+      Console.Beep((int)(ton * 392), (int)(times * 125));
+      Thread.Sleep(250);
+      Console.Beep((int)(ton * 330), (int)(times * 125));
+      Thread.Sleep(250);
+      Console.Beep((int)(ton * 440), (int)(times * 125));
+      Thread.Sleep((int)(times * 125));
+      Console.Beep((int)(ton * 494), (int)(times * 125));
+      Thread.Sleep((int)(times * 125));
+      Console.Beep((int)(ton * 466), (int)(times * 125));
+      Thread.Sleep(42);
     }
   }
 }
